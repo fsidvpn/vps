61d901926c9d20ec7399fb8611ceae0136a73853
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-02-26 23:17:34 +0700

    Add files via upload

---
+++ update/v2raay.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+yl='\e[32;1m'
+bl='\e[36;1m'
+gl='\e[32;1m'
+rd='\e[31;1m'
+mg='\e[0;95m'
+blu='\e[34m'
+op='\e[35m'
+or='\033[1;33m'
+bd='\e[1m'
+color1='\e[031;1m'
+color2='\e[34;1m'
+color3='\e[0m'
+clear 
+cat /usr/bin/bannerku | lolcat
+echo -e ""
+dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
+#########################
+
+BURIQ () {
+    curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow > /root/tmp
+    data=( `cat /root/tmp | grep -E "^### " | awk '{print $2}'` )
+    for user in "${data[@]}"
+    do
+    exp=( `grep -E "^### $user" "/root/tmp" | awk '{print $3}'` )
+    d1=(`date -d "$exp" +%s`)
+    d2=(`date -d "$biji" +%s`)
+    exp2=$(( (d1 - d2) / 86400 ))
+    if [[ "$exp2" -le "0" ]]; then
+    echo $user > /etc/.$user.ini
+    else
+    rm -f /etc/.$user.ini > /dev/null 2>&1
+    fi
+    done
+    rm -f /root/tmp
+}
+
+MYIP=$(curl -sS ipinfo.io/ip)
+Name=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $2}')
+echo $Name > /usr/local/etc/.$Name.ini
+CekOne=$(cat /usr/local/etc/.$Name.ini)
+
+Bloman () {
+if [ -f "/etc/.$Name.ini" ]; then
+CekTwo=$(cat /etc/.$Name.ini)
+    if [ "$CekOne" = "$CekTwo" ]; then
+        res="Expired"
+    fi
+else
+res="Permission Accepted..."
+fi
+}
+
+PERMISSION () {
+    MYIP=$(curl -sS ipinfo.io/ip)
+    IZIN=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | awk '{print $4}' | grep $MYIP)
+    if [ "$MYIP" = "$IZIN" ]; then
+    Bloman
+    else
+    res="Permission Denied!"
+    fi
+    BURIQ
+}
+red='\e[1;31m'
+green='\e[1;32m'
+NC='\e[0m'
+green() { echo -e "\\033[32;1m${*}\\033[0m"; }
+red() { echo -e "\\033[31;1m${*}\\033[0m"; }
+PERMISSION
+
+if [ -f /home/needupdate ]; then
+red "Your script need to update first !"
+exit 0
+elif [ "$res" = "Permission Accepted..." ]; then
+echo -ne
+else
+red "Permission Denied!"
+exit 0
+fi
+clear
+PERMISSION
+
+if [ "$res" = "Expired" ]; then
+Exp="\033[1;31mExpired\033[0m"
+rm -f /home/needupdate > /dev/null 2>&1
+else
+Exp=$(curl -sS https://raw.githubusercontent.com/geovpn/perizinan/main/main/allow | grep $MYIP | awk '{print $3}')
+fi
+chck_b(){
+	PID=`ps -ef |grep -v grep | grep scvps_bot |awk '{print $2}'`
+	if [[ ! -z "${PID}" ]]; then
+			sts="\033[0;32m◉ \033[0m"
+		else
+			sts="\033[1;31m○ \033[0m"
+    fi
+}
+echo -e ""
+ISP=$(curl -s ipinfo.io/org | cut -d " " -f 2-10 )
+#DOMAIN=$(cat /etc/xray/domain)
+CITY=$(curl -s ipinfo.io/city )
+#RAM
+tram=$( free -m | awk 'NR==2 {print $2}' )
+echo -e ""
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e "\E[44;1;39m                     ⇱ INFORMASI VPS ⇲                        \E[0m"
+echo -e "${CYAN}━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m${NC}"
+echo -e " ❇️$bd ISP Name          ${color1} •${color3}$bd $ISP"
+echo -e " ❇️$bd City              ${color1} •${color3}$bd $CITY"
+echo -e " ❇️$bd Total RAM         ${color1} •${color3}$bd $tram MB"
+echo -e " ❇️$bd IP VPS            ${color1} •${color3}$bd $MYIP"
+echo -e " ❇️$bd DOMAIN            ${color1} •${color3}$bd $DOMAIN"
+echo -e " ❇️$bd Client Name       ${color1} •${color3}$bd $Name"
+echo -e " ❇️$bd Expiry script     ${color1} •${color3}$bd $Exp"
+echo -e "${CYAN}
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e "          \e[1;31m\e[1;31m═[\e[mAll Account V2Ray\e[1;31m]═\e[m" 
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e " 1\e[1;33m)\e[m  Create Vmess Websocket Account"
+echo -e " 2\e[1;33m)\e[m  Delete Vmess Websocket Account"
+echo -e " 3\e[1;33m)\e[m  Renew Vmess Account"
+echo -e " 4\e[1;33m)\e[m  Check User Login Vmess"
+echo -e ""
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e " x)   MENU"
+echo -e "\e[1;32m═══════════════════════════════════════" | lolcat
+echo -e ""
+read -p "     Please Input Number  [1-4 or x] :  "  v2ray
+echo -e ""
+case $v2ray in
+1)
+addvmess
+;;
+2)
+delvmess
+;;
+3)
+renewvmess
+;;
+4)
+cekvmess
+;;
+x)
+menu
+;;
+*)
+echo "Please enter an correct number"
+;;
+esac
