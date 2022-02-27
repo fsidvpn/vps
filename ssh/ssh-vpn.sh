3df36468f33f732b16837b760957d1321fc0fab6
fsidvpn <97771305+fsidvpn@users.noreply.github.com>
2022-02-27 11:29:14 +0700

    Update ssh-vpn.sh

--- ssh/ssh-vpn.sh
+++ ssh/ssh-vpn.sh
@@ -3,11 +3,11 @@
 # Color
 RED='\033[0;31m'
 NC='\033[0m'
-#GREEN='\033[0;32m'
-#ORANGE='\033[0;33m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
 BLUE='\033[0;34m'
 PURPLE='\033[0;35m'
-#CYAN='\033[0;36m'
+CYAN='\033[0;36m'
 LIGHT='\033[0;37m'
 off='\x1b[m'
 # ==========================================
@@ -569,6 +569,7 @@ chmod +x deltrgo
 chmod +x renewtrgo
 chmod +x cektrgo
 chmod +x running
+
 echo "0 5 * * * root clearlog && reboot" >> /etc/crontab
 echo "0 0 * * * root xp" >> /etc/crontab
 # remove unnecessary files
